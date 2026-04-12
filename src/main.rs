0a0c1a8b6f4c57134d170afec9926d3b7358e8d8
Blxee <aminedev57@gmail.com>
2026-04-12 21:32:20 +0100

    Added: basic crossterm skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Result, Stdout, stdout};
+
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, read},
+    execute,
+    style::Print,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, size},
+};
+
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen)?;
+    let (cols, rows) = size()?;
+
+    execute!(stdout, Print(format!("hello world {cols}, {rows}")))?;
+
+    while let Event::Key(key) = read()? {
+        if key.kind == KeyEventKind::Press {
+            match key.code {
+                KeyCode::Char('q') => break,
+                _ => (),
+            }
+        }
+    }
+
+    execute!(stdout, LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
