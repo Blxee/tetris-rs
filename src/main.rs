b74a98ef7f7464a07f5d3bf108994a61372dc573
Blxee <aminedev57@gmail.com>
2026-04-13 06:05:57 +0100

    Changed: the main loop and input handling is more solid now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::io::{Result, Stdout, stdout};
+use std::{
+    io::{Result, Stdout, stdout},
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
 use crossterm::{
-    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, read},
+    cursor::{self, Hide, MoveTo, Show},
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, poll, read},
     execute,
     style::Print,
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, size},
@@ -11,19 +16,35 @@ fn main() -> Result<()> {
     let mut stdout = stdout();
     terminal::enable_raw_mode()?;
     execute!(stdout, EnterAlternateScreen)?;
+    execute!(stdout, Hide)?;
     let (cols, rows) = size()?;
+    let program_start = Instant::now();
+    let mut last_frame_time = program_start;
 
-    execute!(stdout, Print(format!("hello world {cols}, {rows}")))?;
-
-    while let Event::Key(key) = read()? {
-        if key.kind == KeyEventKind::Press {
-            match key.code {
-                KeyCode::Char('q') => break,
-                _ => (),
+    loop {
+        let delta = last_frame_time.elapsed();
+        if matches!(poll(Duration::from_secs(0)), Ok(true)) {
+            if let Event::Key(key) = read()? {
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => break,
+                        _ => (),
+                    }
+                }
             }
         }
+        // do whatevee the hell you want here ;b
+        execute!(
+            stdout,
+            MoveTo(cols / 3, rows / 2),
+            Print(format!("delta: {}", delta.as_secs_f32()))
+        )?;
+
+        sleep(Duration::from_secs_f32(1. / 60.));
+        last_frame_time = Instant::now();
     }
 
+    execute!(stdout, Show)?;
     execute!(stdout, LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
     Ok(())
